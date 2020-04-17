a8c0686570bde355b7eb658daf3dfd54191bc96b
Michanowski9 <m.sojka99@gmail.com>
2020-04-17 18:42:14 +0200

    Window Class update

--- Snake/Window.cpp
+++ Snake/Window.cpp
@@ -1,1 +1,14 @@
 #include "Window.h"
+
+Window::Window()
+{
+	Setup("Window", sf::Vector2u(640, 480));
+}
+
+Window::Window(const std::string &l_title, const sf::Vector2u &l_size) 
+{
+	m_windowSize = l_size;
+	m_windowTitle = l_title;
+	Setup(l_title, l_size);
+}
+
--- Snake/Window.h
+++ Snake/Window.h
@@ -1,5 +1,29 @@
 #pragma once
+
+#include <string>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 class Window
 {
+public:
+	Window();
+	Window(const std::string &l_title, const sf::Vector2u &l_size);
+	~Window();
+
+	void BeginDraw();
+	void EndDraw();
+
+	void Update();
+
+	void Draw(sf::Drawable &l_drawable);
+private:
+	void Setup(const std::string &l_title, const sf::Vector2u &l_size);
+	void Create();
+	void Destroy();
+
+	sf::RenderWindow m_window;
+	sf::Vector2u m_windowSize;
+	std::string m_windowTitle;
 };
 
